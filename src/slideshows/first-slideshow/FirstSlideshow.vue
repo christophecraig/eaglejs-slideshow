9435059d3a62dd0507c1a8d379f83657e17639b6
Christophe CRAIG <ccraig@sigb.net>
2017-08-17 17:34:11 +0200

    removed my new slideshow as it caused bugs and wasn't necessary

--- src/slideshows/first-slideshow/FirstSlideshow.vue
+++ src/slideshows/first-slideshow/FirstSlideshow.vue
@@ -1,20 +1,23 @@
 <template lang='pug'>
 #MyFirstSlideshow
   .eg-slideshow
-    slide
+    slide(leave='fadeOut')
       h1 Faciliter la gestion d'une équipe dans le domaine du web
       h4 This is your first slideshow !
 
-    slide(enter='bounceInRight' leave='fadeOut')
+    slide(enter='fadeIn' leave='fadeOut')
       h3 Hey modify me !
       p.
         Come on modify me ! If you are running the development server,
         you will see the changes take effect immediaely
 
-    slide(enter='fadeIn')
+    slide(enter='fadeIn' leave='fadeOut')
       h3 Want cool effects?
       p.
         Code your own, or try stealing for the other slideshows !
+
+    slide(enter='fadeIn')
+      h3 
 </template>
 
 <script>
@@ -29,18 +32,18 @@ export default {
   },
   infos: {
     // These infos appear on the home page, below the slideshow's thumbnail
-    title: 'Your First Slideshow',
-    description: 'A boilerplate to get you started',
-    path: 'your-first-slideshow'
+    title: 'Présentation soutenance PFE',
+    description: 'Faciliter la gestion d\'une équipe dans le domaine du web',
+    path: 'presentation'
   }
 }
 </script>
 
 <style lang='scss'>
-@import url(https://fonts.googleapis.com/css?family=Raleway);
+@import url('https://fonts.googleapis.com/css?family=Rubik');
 #MyFirstSlideshow {
   .eg-slideshow {
-    font-family: 'Raleway';
+    font-family: 'Rubik';
     .eg-slide {
       .eg-slide-content {
         width: 25em;
@@ -56,6 +59,10 @@ export default {
     width: 25em;
     max-width: 80%;
     margin: 0 auto;
+  }
+  .fadeOut {
+    position: absolute;
+    top: 0;
   }
 }
 </style>
